7f32b4c18f2955dea4b6d4363099178aab8ff507
LeviKapllani <33037607+LeviKapllani@users.noreply.github.com>
2019-03-10 16:17:21 -0400

    modified new solutions

--- newSolutions.py
+++ newSolutions.py
@@ -1,39 +1,49 @@
 from swappingParaphrases import *
+import os
+import errno
+import shutil
 
 
-file1 = "BS1.cpp"
-file2 = "BS2.cpp"
+def newSolutions(file1,file2,dirName,combinationNumber):
 
+    filePhraseDict = {}
+    fileNumber = 1
 
-pairs = []
-filePhraseDict = {}
-fileNumber = 3;
+    f1sentences = getFileLines(file1)
+    f2sentences = getFileLines(file2)
 
-f1sentences = getFileLines(file1)
-f2sentences = getFileLines(file2)
+    pairs = SharedSmallestLevenshteinPairs(f1sentences, f2sentences)
 
-pairs = SharedSmallestLevenshteinPairs(f1sentences, f2sentences)
+    try:
+        os.mkdir(dirName)
+    except OSError as exc:
+        if exc.errno != errno.EEXIST:
+            raise
+        pass
 
+    for pair in pairs:
+        filePhraseDict[pair[0]] = file1
+        filePhraseDict[pair[1]] = file2
 
-for pair in pairs:
-    filePhraseDict[pair[0]] = file1
-    filePhraseDict[pair[1]] = file2
+        newPOneValues, newPTwoValues =  newPhrases(pair[0],pair[1])
+        phrases = [newPOneValues, newPTwoValues]
 
-    newPOneValues, newPTwoValues =  newPhrases(pair[0],pair[1])
-    phrases = [newPOneValues, newPTwoValues]
-
-    if newPOneValues != []:
-        for i,phrase in enumerate(phrases):
-            if ''.join(map(str, phrase)) != pair[i]:
-                currentFile = filePhraseDict[pair[i]];
-
-                with open(currentFile) as f:
-                    with open("BS" + str(fileNumber) +".cpp", "w") as f1:
-                        for line in f:
-                            if line.lstrip().strip('\n') == pair[i]:
-                                f1.write(''.join(map(str, phrase)) + '\n')
-                            else:
-                                f1.write(line)
-                    fileNumber += 1
-                    f1.close()
-                    f.close()
+        if newPOneValues != []:
+            for i,phrase in enumerate(phrases):
+                newPhase = ''.join(map(str, phrase)).replace(" ", "")
+                currentPhase = pair[i].replace(" ", "")
+                if newPhase != currentPhase :
+                    currentFile = filePhraseDict[pair[i]];
+                    shutil.copy(currentFile, dirName)
+                    with open(currentFile) as f:
+                        filename = 'solution' + str(combinationNumber) + '-' + str(fileNumber)+".cpp"
+                        filepath = os.path.join(dirName, filename)
+                        with open(filepath, "w") as f1:
+                            for line in f:
+                                if line.lstrip().strip('\n') == pair[i]:
+                                    f1.write(' '.join(map(str, phrase)) + '\n')
+                                else:
+                                    f1.write(line)
+                        fileNumber += 1
+                        f1.close()
+                        f.close()
